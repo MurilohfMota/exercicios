377985f1c30aef699ff3201f1e6bbe46f628318b
Murilo Mota <murilohfmota@gmail.com>
2022-12-03 14:28:07 -0300

    exercicios 1 feitos

--- exercicio01/Q02.java
+++ exercicio01/Q02.java
@@ -1,7 +1,14 @@
 package exercicio01;
-
+import java.util.Scanner;
 public class Q02 {
   public static void main(String[] args) {
-  System.out.println("hellow world");   
+  System.out.println("ponto a :");   
+  Scanner Sc1 = new Scanner(System.in);
+   var a = Sc1.nextDouble();
+   System.out.println("ponto b");
+   var b = Sc1.nextDouble();
+    System.out.println("valor de x :");
+    var x = Sc1.nextDouble();
+    System.out.println("O valor de y é : "+((a*x)+b));
 }
 }
